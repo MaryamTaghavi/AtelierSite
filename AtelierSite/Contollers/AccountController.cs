e2fcf4c07eebeefff868b44eb0f265d1901dc092
ftm-jafari <100162308+ftm-jafari@users.noreply.github.com>
2023-06-22 12:46:53 +0330

    ...

--- AtelierSite/Contollers/AccountController.cs
+++ AtelierSite/Contollers/AccountController.cs
@@ -4,7 +4,23 @@ namespace AtelierSite.Contollers
 {
 	public class AccountController : Controller
 	{
-		public IActionResult Index()
+		public IActionResult Login(string returnUrl = null)
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Login()
+		{
+			return View();
+		}
+		public IActionResult Logout()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Logout(string returnUrl = null)
 		{
 			return View();
 		}
