b8ced10d1ba6f5c2ed433339d23deccadeae982f
ftm-jafari <100162308+ftm-jafari@users.noreply.github.com>
2023-06-26 10:22:42 +0330

    home search

--- AtelierSite/Startup.cs
+++ AtelierSite/Startup.cs
@@ -30,6 +30,7 @@ namespace AtelierSite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddRazorPages();
 
             services.AddDbContext<AtelierContext>(options =>
